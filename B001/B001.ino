7c5ed5598ac721adc0b81ff9940adc1e3380f54a
ray47098607 <git@github.com:ray47098607/vsc0de.git>
2023-12-21 11:13:57 +0800

    01

--- B001/B001.ino
+++ B001/B001.ino
@@ -32,40 +32,31 @@ PZEM004Tv30 pzem(pzemSWSerial);
 
 
 void setup() {
-	Serial.begin(115200);
-
+	Serial.print("Custom Address:");
+    Serial.print(pzem.readAddress(), HEX);
+  	Serial.begin(115200);
+	Serial.println("Humidity,Temp,Voltage,Power,Energy,Frequency,PF");
 
 }
 
 void loop() {
-
-Serial.println("--");
-Serial.println(mills());
-
+//DHT
 	float h = dht.readHumidity();//讀取濕度
 	float t = dht.readTemperature();//讀取攝氏溫度
 	float f = dht.readTemperature(true);//讀取華氏溫度
-    if (isnan(h) || isnan(t) || isnan(f)) {
-		lcd.clear();  //清除螢幕
-		lcd.setCursor(0, 0);   //游標移到左上角
-		lcd.println("DHT ERROR!!");
-		return;
-	}
+    
 	char lineHum[100];//濕度
 	char lineTemp[100];//濕度
 
-	sprintf(lineHum, "Humidity: %d.%02d%%", (int)h, (int)(h * 100) % 100);
-	sprintf(lineTemp, "Temp:   %d.%02d *c", (int)t, (int)(t * 100) % 100);
+	sprintf(lineHum, "%d.%02d%%,", (int)h, (int)(h * 100) % 100);
+	sprintf(lineTemp, "%d.%02d,", (int)t, (int)(t * 100) % 100);
 
 	Serial.print(lineHum);
-	Serial.print("   ");
-	Serial.println(lineTemp);
+	Serial.print(lineTemp);
 
 
 //PZEM004Tv30
-	Serial.print("Custom Address:");
-    Serial.println(pzem.readAddress(), HEX);
-  
+	
       // Read the data from the sensor
      float voltage = pzem.voltage();
       float current = pzem.current();
@@ -88,14 +79,20 @@ Serial.println(mills());
       } else if (isnan(pf)) {
           Serial.println("Error reading power factor");
       } else {
-  
+        Serial.print(voltage);      Serial.print(",");
+        Serial.print(current);      Serial.print(",");
+        Serial.print(power);        Serial.print(",");
+        Serial.print(energy);		Serial.print(",");
+        Serial.print(frequency, 1); Serial.print(",");
+        Serial.print(pf);
+      
           // Print the values to the Serial console
-          Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
+          /*Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
           Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
           Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
           Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
           Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
-          Serial.print("PF: ");           Serial.println(pf);
+          Serial.print("PF: ");           Serial.println(pf);*/
       }
   
       Serial.println();
