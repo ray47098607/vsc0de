bccf61262c6f2d9ead7436c6ab0fd5aeead141c0
ray47098607 <git@github.com:ray47098607/vsc0de.git>
2023-12-21 10:58:05 +0800

    000

--- B001/B001.ino
+++ B001/B001.ino
@@ -1,7 +1,9 @@
+//DHT
 #include <Adafruit_Sensor.h> //DHT先導
 #include <DHT_U.h> 
 #include <DHT.h>
-#define dhtPin 37
+
+#define dhtPin 4
 #define dhtType DHT11
 DHT dht(dhtPin, dhtType); // Initialize DHT sensor
 
@@ -9,18 +11,34 @@ float h = dht.readHumidity();//讀取濕度
 float t = dht.readTemperature();//讀取攝氏溫度
 float f = dht.readTemperature(true);//讀取華氏溫度
 
+//PZEM004Tv30
+#include <PZEM004Tv30.h>
+#include <SoftwareSerial.h>
+
+#if defined(ESP32)
+    #error "Software Serial is not supported on the ESP32"
+#endif
+
+#if !defined(PZEM_RX_PIN) && !defined(PZEM_TX_PIN)
+#define PZEM_RX_PIN 12
+#define PZEM_TX_PIN 13
+#endif
+
+SoftwareSerial pzemSWSerial(PZEM_RX_PIN, PZEM_TX_PIN);
+PZEM004Tv30 pzem(pzemSWSerial);
+
+
+
+
 
 void setup() {
-    	Serial.begin(600);
+	Serial.begin(115200);
 
-pinMode(LED_BUILTIN, OUTPUT);
+
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+
 Serial.println("--");
 Serial.println(mills());
 
@@ -43,4 +61,43 @@ Serial.println(mills());
 	Serial.print("   ");
 	Serial.println(lineTemp);
 
+
+//PZEM004Tv30
+	Serial.print("Custom Address:");
+    Serial.println(pzem.readAddress(), HEX);
+  
+      // Read the data from the sensor
+     float voltage = pzem.voltage();
+      float current = pzem.current();
+      float power = pzem.power();
+      float energy = pzem.energy();
+      float frequency = pzem.frequency();
+      float pf = pzem.pf();
+  
+      // Check if the data is valid
+      if(isnan(voltage)){
+          Serial.println("Error reading voltage");
+      } else if (isnan(current)) {
+          Serial.println("Error reading current");
+      } else if (isnan(power)) {
+          Serial.println("Error reading power");
+      } else if (isnan(energy)) {
+          Serial.println("Error reading energy");
+      } else if (isnan(frequency)) {
+          Serial.println("Error reading frequency");
+      } else if (isnan(pf)) {
+          Serial.println("Error reading power factor");
+      } else {
+  
+          // Print the values to the Serial console
+          Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
+          Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
+          Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
+          Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
+          Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
+          Serial.print("PF: ");           Serial.println(pf);
+      }
+  
+      Serial.println();
+      delay(2000);
 }
